ca88687159a120de1b25f5379454d06bcc7d6e6e
Chris B. Behrens <cbehrens@gmail.com>
2022-03-21 16:41:56 -0500

    Refactor to Action and Function (m02-05)

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,23 +4,23 @@ namespace Delegates
 {
     internal class Program
     {
-        public delegate void WriteMessageFunction(string message);
-
-        public static void WriteMessage(string message)
+        public static bool WriteMessage(string message)
         {
             Console.WriteLine(message);
+            return true;
         }
 
-        public static void WriteTimelyMessage(string message)
+        public static bool WriteTimelyMessage(string message)
         {
             Console.WriteLine(message + " " + DateTime.Now);
+            return true;
         }
 
         static void Main(string[] args)
         {
             var selection = Console.ReadLine();
 
-            WriteMessageFunction func;
+            Func<string, bool> func;
 
             if (selection == "1")
             {
@@ -31,7 +31,12 @@ namespace Delegates
                 func = WriteMessage;
             }
 
-            func("Hello World");
+            var result =    ExecuteWrite(func);
+        }
+
+        private static bool ExecuteWrite(Func<string, bool> func)
+        {
+            return func("Hello World");
         }
     }
 }
