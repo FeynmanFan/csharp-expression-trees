b951be9e0a71d2d8d257960f005d10fd6931df51
Chris B. Behrens <cbehrens@gmail.com>
2022-03-21 14:55:51 -0500

    Initial add

---
+++ Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        public delegate void WriteMessageFunction(string message);
+
+        public static void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void WriteTimelyMessage(string message)
+        {
+            Console.WriteLine(message + " " + DateTime.Now);
+        }
+
+        static void Main(string[] args)
+        {
+            var selection = Console.ReadLine();
+
+            WriteMessageFunction func;
+
+            if (selection == "1")
+            {
+                func = WriteTimelyMessage;
+            }
+            else
+            {
+                func = WriteMessage;
+            }
+
+            func("Hello World");
+        }
+    }
+}
